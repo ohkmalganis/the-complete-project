7cf001726bce036b89e6e666d5b1bb145c874e6d
Lenin Aparicio <ohkmalganis@gmail.com>
2022-06-13 10:23:27 -0400

    Adding a manual observable

--- src/app/home-page/pages/home/rxjs-call/rxjs-call.component.ts
+++ src/app/home-page/pages/home/rxjs-call/rxjs-call.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Subscription, Observable, Observer } from 'rxjs';
 import {HomePageService} from "../../../services/home-page.service";
 import {MessageInterface} from "../../../models/message.interface";
+import { filter, map } from "rxjs/operators";
 
 @Component({
   selector: 'app-rxjs-call',
@@ -33,13 +34,20 @@ export class RxjsCallComponent implements OnInit, OnDestroy {
       setInterval(() => {
         observer.next(count);
         count ++;
-        if(count > 10) {
+        if(count == 11) {
           observer.complete();
+        }
+        if(count > 11) {
+          observer.error(new Error('The count is bad!'));
         }
       }, 1000);
     });
-    this.subscription = customIntervalObservable.subscribe(data => {
+    this.subscription = customIntervalObservable.pipe(filter(data => {
+      return data > 3;
+    })).subscribe(data => {
       this.counter = data;
+    }, (err:Error) => {
+      console.log(err);
     })
   }
 
